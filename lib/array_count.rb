fccfb0be41213ecc9f830f33ecc4fba12dec0734
observant-mapping-4330 <sdonohue0918@gmail.com>
2020-08-05 15:11:08 +0000

    Automatically backed up by Learn

--- lib/array_count.rb
+++ lib/array_count.rb
@@ -5,7 +5,7 @@ array = [1, "hello", [], 5.01, "world", :name, { a: 1 }]
 
 def count_strings(array)
   # Return the total number of strings in the provided array using the count enumerable
-
+  array.count {|el, str| el.is_a? String}
 
 
 
