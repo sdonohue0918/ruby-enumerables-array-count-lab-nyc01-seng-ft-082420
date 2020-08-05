bc8b8c7095f158aa8b88a2dfd29c17ab1abfe04c
observant-mapping-4330 <sdonohue0918@gmail.com>
2020-08-05 15:16:16 +0000

    Done.

--- lib/array_count.rb
+++ lib/array_count.rb
@@ -1,7 +1,18 @@
+array = [1, "hello", [], 5.01, "world", :name, { a: 1 }]
+
+
+
+
 def count_strings(array)
   # Return the total number of strings in the provided array using the count enumerable
+  array.count {|el, str| el.is_a? String}
+
+
+
 end
 
 def count_empty_strings(array)
   # Return the total number of EMPTY strings in the provided array using the count enumerable
+
+   array.count {|el| el == ""}
 end
